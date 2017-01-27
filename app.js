663758f5658b7d572ab6529cb4e7be95f2148103
Noelphilip1990 <noyo1000gt@gmail.com>
2017-01-27 10:34:16 +0530

    Add files via upload
    js file

---
+++ app.js
@@ -0,0 +1,14 @@
+var Employee=[
+
+{name:'Noel',age:26,salary:'12000',city:'Cochin',state:'kerala',pincode:'682024'},
+{name:'Susheel',age:29,salary:'20000',city:'Banglore',state:'Karnataka',pincode:'582005'},
+{name:'Joe',age:35,salary:'40000',city:'wiamoing',state:'NY',pincode:'200053'},
+{name:'Susy',age:25,salary:'7000',city:'agra',state:'New Delhi',pincode:'542003'},
+{name:'Jose',age:24,salary:'18000',city:'cochin',state:'kerala',pincode:'682029'},
+
+];
+
+for(var i=0;i<Employee.length;i++)
+{
+	console.log(Employee[i].name,Employee[i].age,Employee[i].salary,Employee[i].city,Employee[i].state,Employee[i].pincode);
+}
